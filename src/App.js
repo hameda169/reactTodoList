48edbfdfac5700f3185af9ba958859142c120039
hameda169 <169hamed@gmail.com>
2020-01-26 00:20:45 +0330

    5-(all, done, undone) button for presentation added. add new task bug fixed(reset name after adding new task)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 const my_todo_list = [
-  { id: 1, name: "Get Milk", done: false },
-  { id: 2, name: "Go to Gym", done: false },
-  { id: 3, name: "Call Mom", done: false },
-  { id: 4, name: "Work", done: false },
-  { id: 5, name: "Send Email", done: false }
+  { id: 1, name: 'Get Milk', done: false },
+  { id: 2, name: 'Go to Gym', done: false },
+  { id: 3, name: 'Call Mom', done: false },
+  { id: 4, name: 'Work', done: false },
+  { id: 5, name: 'Send Email', done: false }
 ];
 const TodoItem = ({ id, name, remove, done, check }) => (
   <div style={{ width: null }}>
-    <input type={"checkbox"} onClick={() => check(id)} />
-    <span>{id}</span> - <span>{name}</span> -{" "}
-    <span>{done ? "done" : "undone"}</span>
+    <input checked={done} type={'checkbox'} onClick={() => check(id)} />
+    <span>{id}</span> - <span>{name}</span> - <span>{done ? 'done' : 'undone'}</span>
     <button onClick={() => remove(id)}>delete</button>
   </div>
 );
 
 function App() {
   const [tasks, setTasks] = useState(my_todo_list);
-  const [newTask, setNewTask] = useState("");
+  const [newTask, setNewTask] = useState('');
   const [lastId, setLastId] = useState(6);
+  const [show, setShow] = useState('all');
   const remove = id => {
     setTasks(tasks.filter(x => x.id !== id));
   };
@@ -29,24 +29,27 @@ function App() {
   };
   return (
     <div className="App">
-      {tasks.map(x => (
-        <TodoItem
-          id={x.id}
-          name={x.name}
-          remove={remove}
-          check={check}
-          done={x.done}
-        />
-      ))}
-      <input
-        type={"text"}
-        onChange={e => setNewTask(e.target.value)}
-        value={newTask}
-      />
+      <button disabled={show === 'all'} onClick={() => setShow('all')}>
+        All
+      </button>
+      <button disabled={show === 'done'} onClick={() => setShow('done')}>
+        Done
+      </button>
+      <button disabled={show === 'undone'} onClick={() => setShow('undone')}>
+        Undone
+      </button>
+      <br />
+      {tasks
+        .filter(x => (show === 'all' ? true : show === 'done' ? x.done : !x.done))
+        .map(x => (
+          <TodoItem id={x.id} name={x.name} remove={remove} check={check} done={x.done} />
+        ))}
+      <input type={'text'} onChange={e => setNewTask(e.target.value)} value={newTask} />
       <button
         onClick={() => {
           setTasks([...tasks, { id: lastId, name: newTask, done: false }]);
           setLastId(lastId + 1);
+          setNewTask('');
         }}
       >
         Add New Task
