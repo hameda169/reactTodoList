f1adf0701add2f8fdbdbb1b66273fa2d5bfcbb0c
hameda169 <169hamed@gmail.com>
2020-01-26 00:02:07 +0330

    3-Remove any item with delete button

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ const my_todo_list = [
   { id: 4, name: "Work" },
   { id: 5, name: "Send Email" }
 ];
-const TodoItem = ({ id, name }) => (
+const TodoItem = ({ id, name, remove }) => (
   <div style={{ width: null }}>
-    <span>{id}</span> - <span>{name}</span>
+    <span>{id}</span> - <span>{name}</span> -{" "}
+    <button onClick={() => remove(id)}>delete</button>
   </div>
 );
 
@@ -18,10 +19,13 @@ function App() {
   const [tasks, setTasks] = useState(my_todo_list);
   const [newTask, setNewTask] = useState("");
   const [lastId, setLastId] = useState(6);
+  const remove = id => {
+    setTasks(tasks.filter(x => x.id !== id));
+  };
   return (
     <div className="App">
       {tasks.map(x => (
-        <TodoItem id={x.id} name={x.name} />
+        <TodoItem id={x.id} name={x.name} remove={remove} />
       ))}
       <input
         type={"text"}
