1ebc4785edbd684f6c8008cd5acfeb8931f595bf
hameda169 <169hamed@gmail.com>
2020-01-26 00:07:52 +0330

    4-Check and unckeck item (set done and undone tasks)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const my_todo_list = [
-  { id: 1, name: "Get Milk" },
-  { id: 2, name: "Go to Gym" },
-  { id: 3, name: "Call Mom" },
-  { id: 4, name: "Work" },
-  { id: 5, name: "Send Email" }
+  { id: 1, name: "Get Milk", done: false },
+  { id: 2, name: "Go to Gym", done: false },
+  { id: 3, name: "Call Mom", done: false },
+  { id: 4, name: "Work", done: false },
+  { id: 5, name: "Send Email", done: false }
 ];
-const TodoItem = ({ id, name, remove }) => (
+const TodoItem = ({ id, name, remove, done, check }) => (
   <div style={{ width: null }}>
+    <input type={"checkbox"} onClick={() => check(id)} />
     <span>{id}</span> - <span>{name}</span> -{" "}
+    <span>{done ? "done" : "undone"}</span>
     <button onClick={() => remove(id)}>delete</button>
   </div>
 );
@@ -22,10 +24,19 @@ function App() {
   const remove = id => {
     setTasks(tasks.filter(x => x.id !== id));
   };
+  const check = id => {
+    setTasks(tasks.map(x => ({ ...x, done: x.id === id ? !x.done : x.done })));
+  };
   return (
     <div className="App">
       {tasks.map(x => (
-        <TodoItem id={x.id} name={x.name} remove={remove} />
+        <TodoItem
+          id={x.id}
+          name={x.name}
+          remove={remove}
+          check={check}
+          done={x.done}
+        />
       ))}
       <input
         type={"text"}
@@ -34,7 +45,7 @@ function App() {
       />
       <button
         onClick={() => {
-          setTasks([...tasks, { id: lastId, name: newTask }]);
+          setTasks([...tasks, { id: lastId, name: newTask, done: false }]);
           setLastId(lastId + 1);
         }}
       >
