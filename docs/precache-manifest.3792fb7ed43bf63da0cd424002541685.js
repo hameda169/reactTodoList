d734e32540f3bdc58eb2f1ffb2b4c08cdc31c2dc
hameda169 <169hamed@gmail.com>
2020-01-26 10:48:47 +0330

    deployed to github page

---
+++ docs/precache-manifest.3792fb7ed43bf63da0cd424002541685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac95331c1a4d37d1ab57e453df9833f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e63e3a016a2802232e0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "145cafedc8e32107c592",
+    "url": "/static/js/2.33b225ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.33b225ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e63e3a016a2802232e0",
+    "url": "/static/js/main.21e751f9.chunk.js"
+  },
+  {
+    "revision": "47c2d3b3055fbd05840b",
+    "url": "/static/js/runtime-main.27fe3abe.js"
+  }
+]);
