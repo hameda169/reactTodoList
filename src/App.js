5f7023da3f85772608ea11957628990b45ac6bad
hameda169 <169hamed@gmail.com>
2020-02-02 13:49:53 +0330

    some styles applied

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { useCallback, useReducer, useEffect } from "react";
-import "./App.css";
-import { reducer, init } from "./Reducer";
+import { jsx, css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { useCallback, useReducer, useEffect } from 'react';
+import './App.css';
+import { reducer, init } from './Reducer';
 import {
   addTaskAct,
   changeNameAct,
@@ -10,45 +11,50 @@ import {
   initAct,
   removeAct,
   viewAct
-} from "./Actions";
+} from './Actions';
 
 const my_todo_list = [
-  { id: 1, name: "Get Milk", done: false },
-  { id: 2, name: "Go to Gym", done: false },
-  { id: 3, name: "Call Mom", done: false },
-  { id: 4, name: "Work", done: false },
-  { id: 5, name: "Send Email", done: false }
+  { id: 1, name: 'Get Milk', done: false },
+  { id: 2, name: 'Go to Gym', done: false },
+  { id: 3, name: 'Call Mom', done: false },
+  { id: 4, name: 'Work', done: false },
+  { id: 5, name: 'Send Email', done: false }
 ];
 const TodoItem = ({ id, name, remove, done, check }) => (
-  <div style={{ width: null }}>
+  <div css={css`margin:10px;`} onClick={check}>
     <input
       checked={done}
       type="checkbox"
-      onClick={check}
-      css={{ backgroundColor: "#ff0000" }}
+      css={{ backgroundColor: '#ff0000' }}
     />
     <span>{id}</span> - <span>{name}</span>
-    {" - "}
-    <span>{done ? "done" : "undone"}</span>
+    {' - '}
+    <span>{done ? 'done' : 'undone'}</span>
     <button
       onClick={remove}
       css={{
-        fontFamily: "MyFont",
+        fontFamily: 'MyFont',
         borderWidth: 0,
-        backgroundColor: "transparent",
+        backgroundColor: 'transparent',
         fontSize: 16,
-        color: "#5c85c7",
-        transition: "all 0.5s",
-        "&:hover": {
-          color: "#980152",
-          transform: "scale(1.5)"
+        color: '#5c85c7',
+        transition: 'all 0.5s',
+        '&:hover': {
+          color: '#e22b0a',
+          transform: 'scale(1.5)'
         }
       }}
     >
-      {"\ue807"}
+      {'\ue807'}
     </button>
   </div>
 );
+const Select = styled.button({
+  padding: '2px 10px',
+  margin: '5px',
+  fontSize: 16,
+  borderRadius: '10px'
+}, props => ({ backgroundColor: props.disabled ? '#33aa22' : '', color: props.disabled ? '#fff' : '' }));
 
 function App() {
   const [state, dispatch] = useReducer(reducer, init, () => init);
@@ -65,35 +71,62 @@ function App() {
     <div
       className="App"
       css={{
-        background: "linear-gradient(#e6646533, #9198e588)",
-        height: "100%"
+        background: 'linear-gradient(#e6646533, #9198e588)',
+        height: '100%'
       }}
     >
-      <button disabled={state.show === "all"} onClick={changeView("all")}>
+      <Select disabled={state.show === 'all'} onClick={changeView('all')}>
         All
-      </button>
-      <button disabled={state.show === "done"} onClick={changeView("done")}>
+      </Select>
+      <Select disabled={state.show === 'done'} onClick={changeView('done')}>
         Done
-      </button>
-      <button disabled={state.show === "undone"} onClick={changeView("undone")}>
+      </Select>
+      <Select disabled={state.show === 'undone'} onClick={changeView('undone')}>
         Undone
+      </Select>
+      <br/>
+      <div>
+        {state.tasks
+          .filter(x =>
+            state.show === 'all' ? true : state.show === 'done' ? x.done : !x.done
+          )
+          .map(x => (
+            <TodoItem
+              id={x.id}
+              name={x.name}
+              remove={remove(x.id)}
+              check={check(x.id)}
+              done={x.done}
+            />
+          ))}
+      </div>
+      <input
+        type="text"
+        onChange={setNewTaskName}
+        value={state.newTask}
+        css={css`
+          border-width: 0;
+          padding: 5px;
+          font-size: 15px;
+        `}
+      />
+      <button
+        onClick={addNewTask}
+        css={{
+          fontFamily: 'MyFont',
+          borderWidth: 0,
+          backgroundColor: 'transparent',
+          fontSize: 16,
+          color: '#5c85c7',
+          transition: 'all 0.5s',
+          '&:hover': {
+            color: '#2db503',
+            transform: 'scale(2)'
+          }
+        }}
+      >
+        {'\uf0fe'}
       </button>
-      <br />
-      {state.tasks
-        .filter(x =>
-          state.show === "all" ? true : state.show === "done" ? x.done : !x.done
-        )
-        .map(x => (
-          <TodoItem
-            id={x.id}
-            name={x.name}
-            remove={remove(x.id)}
-            check={check(x.id)}
-            done={x.done}
-          />
-        ))}
-      <input type="text" onChange={setNewTaskName} value={state.newTask} />
-      <button onClick={addNewTask}>Add New Task</button>
     </div>
   );
 }
